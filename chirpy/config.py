51185b946723e84529c824a15a51fa0218c5642d
Benjamin Bengfort <benjamin@bengfort.com>
2014-09-26 17:40:14 -0400

    configuration added

---
+++ chirpy/config.py
@@ -0,0 +1,49 @@
+# chirpy.config
+# Uses confire to get meaningful configurations from a yaml file
+#
+# Author:   Benjamin Bengfort <benjamin@bengfort.com>
+# Created:  Fri Sep 19 11:14:33 2014 -0400
+#
+# For license information, see LICENSE.txt
+#
+# ID: config.py [] benjamin@bengfort.com $
+
+"""
+Uses confire to get meaningful configurations from a yaml file
+"""
+
+##########################################################################
+## Imports
+##########################################################################
+
+import os
+import confire
+
+##########################################################################
+## Configuration
+##########################################################################
+
+class ChirpyConfiguration(confire.Configuration):
+    """
+    Meaningful defaults and required configurations.
+
+    debug:    the app will print or log debug statements
+    testing:  the app will not overwrite important resources
+    api_key:  connection information for mongo
+    """
+
+    CONF_PATHS = [
+        "/etc/chirpy.yaml",                      # System configuration
+        os.path.expanduser("~/.chirpy.yaml"),    # User specific config
+        os.path.abspath("conf/chirpy.yaml"),     # Local configuration
+    ]
+
+    debug    = True
+    testing  = True
+    api_key  = None
+
+## Load settings immediately for import
+settings = ChirpyConfiguration.load()
+
+if __name__ == '__main__':
+    print settings
